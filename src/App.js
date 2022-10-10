761bd1d55ace6e77d127ee478990c2a589ed8eb4
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-10-10 22:12:37 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import NotFound from "./components/NotFound/NotFound";
+import Statistics from "./components/Statistics/Statistics";
+import Main from "./layouts/Main";
+import Blogs from "./components/Blogs/Blogs";
 
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "home",
+          element: <Home></Home>,
+        },
+        {
+          path: "statistics",
+          element: <Statistics></Statistics>,
+        },
+        {
+          path: "blogs",
+          element: <Blogs></Blogs>,
+        },
+
+        {
+          path: "about",
+          element: <About></About>,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <NotFound></NotFound>,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [open, setOpen] = useState(false);
+  
+
+  return (
+    <div>
+      <span
+        className="menuBar"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {open ? (
+          <FontAwesomeIcon className="icon" icon={faTimes}></FontAwesomeIcon>
+        ) : (
+          <FontAwesomeIcon className="icon" icon={faBars}></FontAwesomeIcon>
+        )}
+      </span>
+      <nav>
+        <span className="menu-title">Quick Quiz</span>
+        <ul className={`${open ? "showNav" : "hideNav"}`}>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/home"
+          >
+            <li>Home</li>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="statistics"
+          >
+            <li>Statistics</li>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="blogs"
+          >
+            <li>Blogs</li>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="about"
+          >
+            <li>About Us</li>
+          </NavLink>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
