56607794899bbf182dc5efd1ac5a44d3c58c1792
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-10-11 20:36:12 +0600

    not found route added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './NotFound.css';
+import notFoundImg from '../../Images/not-found.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCross } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            
-        </div>
+      <div className="not-found">
+        <h1>☠ 404 ☠</h1>
+        <h4>😥 Sorry, the page not found 😥</h4>
+        <p>
+          The link you followed probably broken or the page has been removed
+        </p>
+        <Link className='not-found-button' to="/home">
+          <button className="header-button">Back To Home</button>
+        </Link>
+      </div>
     );
 };
 
