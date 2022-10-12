a5b98628d3cabfeac122a49c8fc46b5deffe2793
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-10-12 12:55:42 +0600

    delete react toast and ues sweet alert to fix toast problem

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,17 +3,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Option from "../Option/Option";
 import "./Quiz.css";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Swal from "sweetalert2";
 
 const Quiz = ({ ques, quesId, tryResult }) => {
-  const notify = (corAns) => toast(corAns);
   const { options, question, correctAnswer } = ques;
-
+  const notify = (corAns) => {
+    Swal.fire({
+      text: '[correct answer]',
+      title: `${corAns}`,
+      showConfirmButton: true,
+      timer: 1500,
+    });
+  }
   const isCorrect = (opt) => {
     if (opt === correctAnswer) {
-        tryResult(true);
+      tryResult(true);
       Swal.fire({
         icon: "success",
         title: "Congratulations!",
@@ -21,21 +25,20 @@ const Quiz = ({ ques, quesId, tryResult }) => {
         showConfirmButton: true,
         timer: 1500,
       });
-    }else{
-        tryResult(false);
-        Swal.fire({
-          icon: "error",
-          title: "Wrong Answer!",
-          text: "Try Again",
-          showConfirmButton: true,
-          timer: 1500,
-        });
+    } else {
+      tryResult(false);
+      Swal.fire({
+        icon: "error",
+        title: "Wrong Answer!",
+        text: "Try Again",
+        showConfirmButton: true,
+        timer: 1500,
+      });
     }
   };
 
   return (
     <div className="questions-container">
-      <ToastContainer/>
       <section className="quiz-ques">
         <h2>
           <span>Quiz {quesId} :</span> {question}
